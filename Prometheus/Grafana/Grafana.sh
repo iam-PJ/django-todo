ead480bf7a8c56b57e92d47c4b80725a001e32b4
iam-PJ <prathmeshjagtap3636@gmail.com>
2025-02-24 22:38:32 +0530

    Create Grafana.sh

---
+++ Prometheus/Grafana/Grafana.sh
@@ -0,0 +1,3 @@
+sudo apt-get install -y adduser libfontconfig1 musl
+wget https://dl.grafana.com/enterprise/release/grafana-enterprise_11.5.2_amd64.deb
+sudo dpkg -i grafana-enterprise_11.5.2_amd64.deb
